64f5d18a05da06d799cce82a85510cda5fe37d1a
Christian Romney <cromney@pointslope.com>
2013-01-03 00:33:44 -0500

    Added multiinsertR/L and multisubst implementatons
    Signed-off-by: Christian Romney <cromney@pointslope.com>

--- little-schemer/cons-the-magnificent.rkt
+++ little-schemer/cons-the-magnificent.rkt
@@ -12,5 +12,46 @@
                     (multirember a (cdr lat))))))
 
 (multirember 'cup '(coffee cup tea cup and hick cup))
-
 ;; => '(coffee tea and hick)
+
+
+;; pp. 56
+;; Write the function multiinsertR
+;; Finds every occurrence of 'old' in 'lat' and
+;; adds 'new' to the right of 'old'
+(define (multiinsertR new old lat)
+  (cond ((null? lat) '())
+        ((eq? old (car lat)) (cons old
+                                   (cons new
+                                         (multiinsertR new old (cdr lat)))))
+        (else (cons (car lat)
+                    (multiinsertR new old (cdr lat))))))
+
+(multiinsertR 'bad 'big '(big girls like big men))
+;; => '(big bad girls like big bad men)
+
+
+;; pp.57
+;; Write the function multiinsertL
+;; Similar to multiinsertR, only new is inserted
+;; before old instead of after it
+(define (multiinsertL new old lat)
+  (cond ((null? lat) '())
+        ((eq? old (car lat)) (cons new (cons old (multiinsertL new old (cdr lat)))))
+        (else (cons (car lat) (multiinsertL new old (cdr lat))))))
+
+(multiinsertL 'smelly 'sea '(sally sells sea shells by the sea shore))
+;; => '(sally sells smelly sea shells by the smelly sea shore)
+
+
+;; pp. 57
+;; Write the function multisubst
+;; The function replaces all occurrences of 'old'
+;; within 'lat' with the 'new' value
+(define (multisubst new old lat)
+  (cond ((null? lat) '())
+        ((eq? old (car lat)) (cons new (multisubst new old (cdr lat))))
+        (else (cons (car lat) (multisubst new old (cdr lat))))))
+
+(multisubst 'blue 'red '(red bonnet red bonnet))
+;; => '(blue bonnet blue bonnet)
