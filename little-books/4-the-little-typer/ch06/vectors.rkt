df5259fa71eaddc328907f93adbb2d6b5e3c165b
Christian Romney <christian.a.romney@gmail.com>
2019-04-07 09:43:09 -0400

    Use org-mode and org-babel for literate programming
    This lets me take notes with inline code blocks I can export
    with org-babel-tangle and then execute and test.
    
    Signed-off-by: Christian Romney <christian.a.romney@gmail.com>

--- little-books/4-the-little-typer/ch06/vectors.rkt
+++ little-books/4-the-little-typer/ch06/vectors.rkt
@@ -1,4 +1,3 @@
-;; -*- mode: racket; -*-
 #lang pie
 
 (claim first-of-one
@@ -13,5 +12,52 @@
 
 (check-same
  Nat
- 5
+ (the Nat 5)
  (first-of-one Nat (vec:: 5 vecnil)))
+
+(claim first-of-two
+  (Π ((E U))
+     (-> (Vec E 2)
+         E)))
+
+(define first-of-two
+  (λ (E)
+    (λ (v)
+      (head v))))
+
+(check-same
+ Nat
+ (the Nat 5)
+ (first-of-two Nat (vec:: 5 (vec:: 6 vecnil))))
+
+(claim first
+  (Π ((E U)
+      (l Nat))
+     (-> (Vec E (add1 l))
+         E)))
+
+(define first
+  (λ (E l)
+    (λ (v)
+      (head v))))
+
+(check-same
+ Nat
+ (the Nat 2)
+ (first Nat 0 (vec:: 2 vecnil)))
+
+(claim rest
+  (Π ((E U)
+      (l Nat))
+     (-> (Vec E (add1 l))
+         (Vec E l))))
+
+(define rest
+  (λ (E l)
+    (λ (v)
+      (tail v))))
+
+(check-same
+ (Vec Nat 1)
+ (the (Vec Nat 1) (vec:: 2 vecnil))
+ (rest Nat 1 (vec:: 1 (vec:: 2 vecnil))))
