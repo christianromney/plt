9befd8920b6474ecdebadab65a4b121219045bc3
Christian Romney <christian.a.romney@gmail.com>
2019-04-07 11:01:39 -0400

    Completed peas exercise
    Signed-off-by: Christian Romney <christian.a.romney@gmail.com>

--- little-books/4-the-little-typer/ch07/dependent-types.rkt
+++ little-books/4-the-little-typer/ch07/dependent-types.rkt
@@ -1,1 +1,40 @@
 #lang pie
+
+(claim motive-peas
+  (Π ((l Nat))
+     U))
+
+(define motive-peas
+  (λ (l)
+    (Vec Atom l)))
+
+(claim step-peas
+  (Π ((l-1 Nat))
+     (-> (motive-peas l-1)
+         (motive-peas (add1 l-1)))))
+
+(define step-peas
+  (λ (l-1)
+    (λ (answer-l-1)
+      (vec:: 'pea answer-l-1))))
+
+(claim peas
+  (Π ((l Nat))
+     (Vec Atom l)))
+
+(define peas
+  (λ (l)
+    (ind-Nat l
+      motive-peas
+      vecnil
+      step-peas)))
+
+(check-same
+ (Vec Atom 5)
+ (the (Vec Atom 5)
+      (vec:: 'pea
+             (vec:: 'pea
+                    (vec:: 'pea
+                           (vec:: 'pea
+                                  (vec:: 'pea vecnil))))))
+ (peas 5))
